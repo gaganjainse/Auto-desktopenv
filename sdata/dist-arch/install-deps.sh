f9cecc7516d6c6cb2c2dce1838eda9a520db16db
Gagan Jain <gagan.jain.se@gmail.com>
2026-08-17 00:01:01 +0530

    fix: remove deprecated packages without bypassing dependencies

--- sdata/dist-arch/install-deps.sh
+++ sdata/dist-arch/install-deps.sh
@@ -13,38 +13,47 @@ install-yay(){
 }
 
 remove_deprecated_dependencies(){
-  printf "${STY_CYAN}[$0]: Removing deprecated dependencies:${STY_RST}\n"
+  printf "${STY_CYAN}[$0]: Removing deprecated dependencies (dependency-safe):${STY_RST}\n"
   local list=()
   list+=(illogical-impulse-{microtex,pymyc-aur,oneui4-icons-git})
   list+=(hyprland-qtutils)
   list+=({quickshell,hyprutils,hyprpicker,hyprlang,hypridle,hyprland-qt-support,hyprland-qtutils,hyprlock,xdg-desktop-portal-hyprland,hyprcursor,hyprwayland-scanner,hyprland}-git)
   list+=(matugen-bin)
-  for i in "${list[@]}";do try sudo pacman --noconfirm -Rdd "$i";done
+  local pkg
+  for pkg in "${list[@]}"; do
+    if pacman -Q "$pkg" >/dev/null 2>&1; then
+      if sudo pacman -Rns --noconfirm "$pkg"; then
+        printf "${STY_GREEN}[$0]: Removed deprecated package: %s${STY_RST}\n" "$pkg"
+      else
+        printf "${STY_YELLOW}[$0]: Could not remove %s safely because installed dependencies still require it; leaving it in place.${STY_RST}\n" "$pkg"
+      fi
+    fi
+  done
 }
-# NOTE: `implicitize_old_dependencies()` was for the old days when we just switch from dependencies.conf to local PKGBUILDs.
-# However, let's just keep it as references for other distros writing their `sdata/dist-<OS_GROUP_ID>/install-deps.sh`, if they need it.
+
 implicitize_old_dependencies(){
-# Convert old dependencies to non explicit dependencies so that they can be orphaned if not in meta packages
   remove_bashcomments_emptylines ./sdata/dist-arch/previous_dependencies.conf ./cache/old_deps_stripped.conf
   readarray -t old_deps_list < ./cache/old_deps_stripped.conf
   pacman -Qeq > ./cache/pacman_explicit_packages
   readarray -t explicitly_installed < ./cache/pacman_explicit_packages
 
   echo "Attempting to set previously explicitly installed deps as implicit..."
-  for i in "${explicitly_installed[@]}"; do for j in "${old_deps_list[@]}"; do
-    [ "$i" = "$j" ] && yay -D --asdeps "$i"
-  done; done
-
-  return 0
+  for i in "${explicitly_installed[@]}"; do
+    for j in "${old_deps_list[@]}"; do
+      if [[ "$i" == "$j" ]]; then
+        if ! yay -D --asdeps "$i"; then
+          log_warning "Could not mark $i as a dependency; leaving package metadata unchanged."
+        fi
+      fi
+    done
+  done
 }
 
-#####################################################################################
 if ! command -v pacman >/dev/null 2>&1; then
-  printf "${STY_RED}[$0]: pacman not found, it seems that the system is not ArchLinux or Arch-based distros. Aborting...${STY_RST}\n"
+  printf "${STY_RED}[$0]: pacman not found; this installer requires Arch/pacman. Aborting...${STY_RST}\n"
   exit 1
 fi
 
-# Keep makepkg from resetting sudo credentials
 if [[ -z "${PACMAN_AUTH:-}" ]]; then
   export PACMAN_AUTH="sudo"
 fi
@@ -52,16 +61,13 @@ fi
 showfun remove_deprecated_dependencies
 v remove_deprecated_dependencies
 
-# Issue #363
 case $SKIP_SYSUPDATE in
   true) true;;
   *) v sudo pacman -Syu;;
 esac
 
-# Use yay. Because paru does not support cleanbuild.
-# Also see https://wiki.hyprland.org/FAQ/#how-do-i-update
-if ! command -v yay >/dev/null 2>&1;then
-  echo -e "${STY_YELLOW}[$0]: \"yay\" not found.${STY_RST}"
+if ! command -v yay >/dev/null 2>&1; then
+  echo -e "${STY_YELLOW}[$0]: yay not found.${STY_RST}"
   showfun install-yay
   v install-yay
 fi
@@ -69,27 +75,17 @@ fi
 showfun implicitize_old_dependencies
 v implicitize_old_dependencies
 
-# https://github.com/end-4/dots-hyprland/issues/581
-# yay -Bi is kinda hit or miss, instead cd into the relevant directory and manually source and install deps
 install-local-pkgbuild() {
   local location=$1
   local installflags=$2
 
-  x pushd $location
-
+  x pushd "$location"
   source ./PKGBUILD
   x yay -S --sudoloop $installflags --asdeps "${depends[@]}"
-  # man makepkg:
-  # -A, --ignorearch: Ignore a missing or incomplete arch field in the build script.
-  # -s, --syncdeps: Install missing dependencies using pacman. When build-time or run-time dependencies are not found, pacman will try to resolve them.
-  # -f, --force: build a package even if it already exists in the PKGDEST
-  # -i, --install: Install or upgrade the package after a successful build using pacman(8).
-  # In https://github.com/end-4/dots-hyprland/issues/823#issuecomment-3394774645 it's suggested to use `sudo pacman -U --noconfirm *.pkg.tar.zst` instead of `makepkg -i`, however it's possible that multiple *.pkg.tar.zst exist, which makes this command not reliable.
   x makepkg -Afsi --noconfirm
   x popd
 }
 
-# Install core dependencies from the meta-packages
 metapkgs=(./sdata/dist-arch/illogical-impulse-{audio,backlight,basic,fonts-themes,kde,portal,python,screencapture,toolkit,widgets})
 metapkgs+=(./sdata/dist-arch/illogical-impulse-hyprland)
 metapkgs+=(./sdata/dist-arch/illogical-impulse-microtex-git)
@@ -102,24 +98,23 @@ for i in "${metapkgs[@]}"; do
   v install-local-pkgbuild "$i" "$metainstallflags"
 done
 
-# Install display manager (SDDM)
 v sudo pacman -S --needed --noconfirm sddm
-## Optional dependencies
-if pacman -Qs ^plasma-browser-integration$ ;then SKIP_PLASMAINTG=true;fi
+if pacman -Qs ^plasma-browser-integration$ ; then
+  SKIP_PLASMAINTG=true
+fi
 case $SKIP_PLASMAINTG in
   true) true;;
   *)
-    if $ask;then
-      echo -e "${STY_YELLOW}[$0]: NOTE: The size of \"plasma-browser-integration\" is ~600 KiB, but if you don't yet have KDE on your system it'll pull an extra ~600MiB of packages.${STY_RST}"
-      echo -e "${STY_YELLOW}It is needed if you want playtime of media in Firefox to be shown on the music controls widget.${STY_RST}"
+    if $ask; then
+      echo -e "${STY_YELLOW}[$0]: plasma-browser-integration is optional and can add KDE dependencies.${STY_RST}"
       echo -e "${STY_YELLOW}Install it? [y/N]${STY_RST}"
-      read -p "====> " p
+      read -r -p "====> " p
     else
       p=y
     fi
     case $p in
       y) x sudo pacman -S --needed --noconfirm plasma-browser-integration ;;
-      *) echo "Ok, won't install"
+      *) echo "Ok, won't install" ;;
     esac
     ;;
 esac
